8baaa2c2b16ab9fa6e5aa13aca243c735fa6a40e
alot290 <qbs1864@outlook.com>
2019-10-19 16:49:00 -0400

    no more sql

--- index.js
+++ index.js
@@ -1,57 +1,70 @@
-console.log("It works");
+
 
 //Google Charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.load('current', {'packages':['corechart']});
+// google.charts.setOnLoadCallback(drawChart);
+
+
 var test = localStorage.getItem('FirstName');
-console.log(test);
-function drawChart() {
-  var moneyScale=0;
 
-  var data = google.visualization.arrayToDataTable([
-
-
-    //Mock Data
-    ['Year', 'Sales', 'Expenses'],
-    ['2018',   parseInt(test,10) ,      0],
-    ['2019',  100,      0],
-    ['2020',  200,       0],
-    ['2021',  1400,      0]
-  ]);
-  var options = {
-    title: 'Broadview',
-    curveType: 'function',
-    legend: { position: 'right' }
-  };
-
-  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-  chart.draw(data, options);
+function call(){
+  var firstname = document.getElementById('FirstName');/*document.getElementById("firstname");*/
+  var lastname = document.getElementById('LastName');;//document.getElementById("lastname");
+  var source = document.getElementById('Source');;//document.getElementById("source");
+  var type = document.getElementById('Type');;//document.getElementById("type");
+  var amount = document.getElementById("Amount");
+  var interest = document.getElementById("Interest");
+  // console.log(firstname);
+  console.log(document.getElementById('test'));
 }
-//End of Google Charts
-
-
-//alert("HI");
-if (typeof(Storage) !== "undefined") {
-    var firstname = document.getElementById('FirstName');/*document.getElementById("firstname");*/
-    var lastname = document.getElementById('LastName');;//document.getElementById("lastname");
-    var source = document.getElementById('Source');;//document.getElementById("source");
-    var type = document.getElementById('Type');;//document.getElementById("type");
-    var amount = document.getElementById("Amount");
-    var interest = document.getElementById("Interest");
-
-    localStorage.setItem("firstname", firstname);
-    localStorage.setItem("lastname", lastname);
-    localStorage.setItem("source", source);
-    localStorage.setItem("amount", amount);
-    localStorage.setItem("interest", interest);
-    localStorage.setItem("userid", Math.floor((Math.random() * 100000) + 999999));
-
-
-
-} else {
-    table.innerHTML = "Sorry, your browser does not support Web Storage...";
-}
+//
+// function drawChart() {
+//   var moneyScale=0;
+//
+//   var data = google.visualization.arrayToDataTable([
+//
+//
+//     //Mock Data
+//     ['Year', 'Sales', 'Expenses'],
+//     ['2018',   parseInt(test,10) ,      0],
+//     ['2019',  100,      0],
+//     ['2020',  200,       0],
+//     ['2021',  1400,      0]
+//   ]);
+//   var options = {
+//     title: 'Broadview',
+//     curveType: 'function',
+//     legend: { position: 'right' }
+//   };
+//
+//   var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+//
+//   chart.draw(data, options);
+// }
+// //End of Google Charts
+//
+//
+// //alert("HI");
+// if (typeof(Storage) !== "undefined") {
+//     var firstname = document.getElementById('FirstName');/*document.getElementById("firstname");*/
+//     var lastname = document.getElementById('LastName');;//document.getElementById("lastname");
+//     var source = document.getElementById('Source');;//document.getElementById("source");
+//     var type = document.getElementById('Type');;//document.getElementById("type");
+//     var amount = document.getElementById("Amount");
+//     var interest = document.getElementById("Interest");
+//
+//     localStorage.setItem("firstname", firstname);
+//     localStorage.setItem("lastname", lastname);
+//     localStorage.setItem("source", source);
+//     localStorage.setItem("amount", amount);
+//     localStorage.setItem("interest", interest);
+//     localStorage.setItem("userid", Math.floor((Math.random() * 100000) + 999999));
+//
+//
+//
+// } else {
+//     table.innerHTML = "Sorry, your browser does not support Web Storage...";
+// }
 
 
 
