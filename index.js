fe9f2d6fecdefd25a330a56df83fffc50147b10c
alot290 <qbs1864@outlook.com>
2019-10-19 02:04:21 -0400

    added tabs

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 console.log("It works");
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  //Mock Data
+    ['Year', 'Sales', 'Expenses'],
+    ['2018',  1000,      400],
+    ['2019',  1170,      460],
+    ['2020',  0,       1120],
+    ['2021',  0,      540]
+  ]);
+
+  var options = {
+    title: 'Broadview',
+    curveType: 'function',
+    legend: { position: 'bottom' }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
+}
+
+
+
+// Tabs
+function openCity(evt, tabs) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabs).style.display = "block";
+  evt.currentTarget.className += " active";
+}
