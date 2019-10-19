b896fa9350cab1e28ebf968be52baf7f2fc987af
Wesley Fortier <43621975+wfortier1@users.noreply.github.com>
2019-10-19 10:24:51 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql");
+
+let con = mysql.createConnection({
+    host: "35.185.83.199",
+    user: "wes",
+    password: "hackumass",
+    database: "peopledb",
+    port: 3306
+});
+
+con.connect(function(err) {
+  if (err) {
+      console.trace(err);
+      throw err;
+  }
+  console.log("Connected!");
+});
+
+var addPerson = (firstName,lastName,source,type,amount,interest,userID) => {
+    var sqlInsert = "INSERT INTO peopledb (FirstName,LastName,Source,Type,Amount,Interest,UserID) VALUES ?";
+    var values = [firstName,lastName,source,type,amount,interest,userID];
+    con.query(insertSQL, [values] ,function(error) {
+        if (error) throw error;
+        console.log("New person created!");
+    });
+};
