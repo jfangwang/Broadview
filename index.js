4a93bde36167c41c6c1e0e189c8b7f083d0cb7d0
alot290 <qbs1864@outlook.com>
2019-10-19 15:54:52 -0400

    Trying to work with firstname

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ console.log("It works");
 //Google Charts
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
+var test = localStorage.getItem('FirstName');
+console.log(test);
 function drawChart() {
   var moneyScale=0;
+
   var data = google.visualization.arrayToDataTable([
+
+
     //Mock Data
     ['Year', 'Sales', 'Expenses'],
-    ['2018',  1000,      0],
+    ['2018',   parseInt(test,10) ,      0],
     ['2019',  100,      0],
     ['2020',  200,       0],
     ['2021',  1400,      0]
@@ -24,6 +29,35 @@ function drawChart() {
   chart.draw(data, options);
 }
 //End of Google Charts
+
+
+//alert("HI");
+if (typeof(Storage) !== "undefined") {
+    var firstname = document.getElementById('FirstName');/*document.getElementById("firstname");*/
+    var lastname = document.getElementById('LastName');;//document.getElementById("lastname");
+    var source = document.getElementById('Source');;//document.getElementById("source");
+    var type = document.getElementById('Type');;//document.getElementById("type");
+    var amount = document.getElementById("Amount");
+    var interest = document.getElementById("Interest");
+
+    localStorage.setItem("firstname", firstname);
+    localStorage.setItem("lastname", lastname);
+    localStorage.setItem("source", source);
+    localStorage.setItem("amount", amount);
+    localStorage.setItem("interest", interest);
+    localStorage.setItem("userid", Math.floor((Math.random() * 100000) + 999999));
+
+
+
+} else {
+    table.innerHTML = "Sorry, your browser does not support Web Storage...";
+}
+
+
+
+
+
+
 
 
 // Tab Links
