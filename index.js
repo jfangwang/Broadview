7e1d771ae41d94378f192f8d536e631e2c81b0e0
alot290 <qbs1864@outlook.com>
2019-10-20 04:35:03 -0400

    added right and left tabs

--- index.js
+++ index.js
@@ -4,63 +4,56 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 google.charts.setOnLoadCallback(drawBasic);
+google.charts.setOnLoadCallback(drawFuture);
 
-var test = localStorage.getItem('FirstName');
+function drawFuture() {
+  var moneyScale=0;
 
-function call(){
-  var firstname = document.getElementById('FirstName');/*document.getElementById("firstname");*/
-  var lastname = document.getElementById('LastName');;//document.getElementById("lastname");
-  var source = document.getElementById('Source');;//document.getElementById("source");
-  var type = document.getElementById('Type');;//document.getElementById("type");
-  var amount = document.getElementById("Amount");
-  var interest = document.getElementById("Interest");
-  // console.log(firstname);
-  console.log(document.getElementById('test'));
+  var data = google.visualization.arrayToDataTable([
+    //Mock Data
+    ['Year', 'Sales', 'Expenses'],
+    ['2018',   200 ,      0],
+    ['2019',  100,      0],
+    ['2020',  200,       0],
+    ['2021',  1400,      0]
+  ]);
+  var options = {
+    title: 'Broadview',
+    curveType: 'function',
+    legend: { position: 'right' }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('future_chart'));
+
+  chart.draw(data, options);
 }
 
 function drawBasic() {
+  var moneyScale=0;
 
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'X');
-      data.addColumn('number', 'Dogs');
+  var data = google.visualization.arrayToDataTable([
+    //Mock Data
+    ['Year', 'Sales', 'Expenses'],
+    ['2018',   200 ,      0],
+    ['2019',  100,      0],
+    ['2020',  200,       0],
+    ['2021',  1400,      0]
+  ]);
+  var options = {
+    title: 'Broadview',
+    curveType: 'function',
+    legend: { position: 'right' }
+  };
 
-      data.addRows([
-        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-        [66, 70], [67, 72], [68, 75], [69, 80]
-      ]);
+  var chart = new google.visualization.LineChart(document.getElementById('basic_chart'));
 
-      var options = {
-        hAxis: {
-          title: 'Time'
-        },
-        vAxis: {
-          title: 'Popularity'
-        }
-      };
-
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-    }
-
-
+  chart.draw(data, options);
+}
 
 function drawChart() {
   var moneyScale=0;
 
   var data = google.visualization.arrayToDataTable([
-
-
     //Mock Data
     ['Year', 'Sales', 'Expenses'],
     ['2018',   200 ,      0],
@@ -105,13 +98,25 @@ if (typeof(Storage) !== "undefined") {
 
 
 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
+function hide(){
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  var tablinks = document.getElementsByClassName("tablinks");
+    tabcontent[0].style.display = "none";
+    tabcontent[2].style.display = "none";
+}
 
 
 
 // Tab Links
-function openCity(evt, tabs) {
+function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -121,7 +126,7 @@ function openCity(evt, tabs) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(tabs).style.display = "block";
+  document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 //End of Tab Links
